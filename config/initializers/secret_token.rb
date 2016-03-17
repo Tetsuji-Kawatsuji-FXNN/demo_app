83cefb5bf5c57ff33fc674802c2ad9bce19f3b8b
Tetsuji-Kawatsuji-FXNN <tetsuji.kawatsuji@fxnn.fujixerox.co.jp>
2016-03-17 15:12:33 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '67a7c9fa458cbe908f3c432596672e90ce362a704bc85ba41a9a217547287cde8f86f75657d2f04f5416771bb9ca8ec913e3bfad98e7ba8972bbf965648c975b'
